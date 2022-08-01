b411b16639e76b9554de1510076a996b4ab4b167
GoodheadHe <1258066455@qq.com>
2022-08-01 16:45:26 +0800

    新增GameDetailPlus

---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Controllers/GameDetailController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RegisterLogin.Models;
+using RegisterLogin.Helpers;
+
+namespace RegisterLogin.Controllers
+{
+    [Route("api/GetGameCarousel")]
+    [ApiController]
+    public class GameDetailController : Controller
+    {
+        [HttpGet]
+        public GetGameDetailResponse Get([FromBody] GetGameDetailRequest req)
+        {
+            GameDetailPlusHelper gd = new GameDetailPlusHelper();
+            GetGameDetailResponse resp = gd.getGameDetail(req);
+
+            return resp;
+        }
+    }
+}
