d0e231f61ba35c669261b6cc9be11eca369de61b
jjw2606781566 <87257275+jjw2606781566@users.noreply.github.com>
2022-08-15 11:19:15 +0800

    新增logout接口

---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Controllers/logoutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using logout.Helpers;
+using logout.Models;
+using System;
+
+namespace logout.Controllers
+{
+    [Route("api/user/logout")]
+    [ApiController]
+    public class logoutController : Controller
+    {
+        [HttpPost]
+        public logoutResponse Post([FromBody] logoutRequest req)
+        {
+            logoutHelper db = new logoutHelper();
+            logoutResponse resp = db.logout(req);
+
+            return resp;
+        }
+    }
+}
