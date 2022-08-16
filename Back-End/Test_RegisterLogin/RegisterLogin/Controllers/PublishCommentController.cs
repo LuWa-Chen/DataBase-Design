77e23f07ea101b0ec5189eac7f0c05897ab03ab9
jjw2606781566 <87257275+jjw2606781566@users.noreply.github.com>
2022-08-16 14:59:42 +0800

    新增发布评论接口

---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Controllers/PublishCommentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PublishComment.Helpers;
+using PublishComment.Models;
+using System;
+namespace PublishComment.Controllers
+{
+    [Route("api/library/comment")]
+    [ApiController]
+    public class PublishCommentController : Controller
+    {
+        [HttpPost]
+        public PublishCommentResponse Post([FromBody] PublishCommentRequest req)
+        {
+            PublishCommentHelper db = new PublishCommentHelper();
+            PublishCommentResponse resp = db.PublishComment(req);
+
+            return resp;
+        }
+    }
+}
---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Models/PublishComment.cs
@@ -0,0 +1,15 @@
+﻿namespace PublishComment.Models
+{
+    public class PublishCommentRequest
+    {
+        public string user_id { get; set; }
+        public string game_id { get; set; }
+        public int rating { get; set; }
+        public string content { get; set; }
+    }
+    public class PublishCommentResponse
+    {
+        public int result { get; set; }
+        public string messsage { get; set; }
+    }
+}
