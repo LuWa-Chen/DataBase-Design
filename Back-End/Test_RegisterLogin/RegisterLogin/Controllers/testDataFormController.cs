6665019e83aeca461003b4f3c57bf226ba55a321
jjw2606781566 <87257275+jjw2606781566@users.noreply.github.com>
2022-08-17 19:02:53 +0800

    新增了用于测试FormData数据类型的接口

---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Controllers/testDataFormController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using testDataForm.Models;
+using System.IO;
+using System;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+using System.Drawing;
+
+namespace testDataForm.Controllers
+{
+    [Route("api/publisher/publishsource")]
+    [ApiController]
+public class testDataFormController : Controller
+    {
+        [HttpPost]
+        public testDataFormResponse Post([FromForm] testDataFormRequest req)
+        {
+            testDataFormResponse resp = new testDataFormResponse();
+            FileSave(req);
+            Console.WriteLine(Request.Form["teststring"]);
+            resp.result = 1;
+            return resp;
+        }
+        public async Task<IActionResult> FileSave(testDataFormRequest req)
+        {
+            var filename = req.files.Files["MainPicSrc"].FileName;
+            var file = req.files.Files["MainPicSrc"];
+            long fileSize = file.Length;
+            string fileext = Path.GetExtension(filename);
+            string filepath = "C:\\test\\logo" + fileext;
+            var stream = new FileStream(filepath, FileMode.Create);
+            await file.CopyToAsync(stream);
+            stream.Close();
+            return Ok(new { count = req.files.Count });
+        }
+    }
+}
---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Models/testDataForm.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace testDataForm.Models
+{
+    public class testDataFormRequest
+    {
+        public IFormCollection files{ get; set; }
+    }
+    public class testDataFormResponse
+    {
+        public int result { get; set; }
+    }
+}
