64812bb48d6778ca62cf4d539bc19f10feb8e488
GoodheadHe <1258066455@qq.com>
2022-08-26 10:21:28 +0800

    新增将通过购物车购买的游戏加入用户库接口

---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Helpers/GameAdd2LibHelper.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using RegisterLogin.Models;
+
+namespace RegisterLogin.Helpers
+{
+    public class GameAdd2LibHelper
+    {
+        public static string connString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=139.196.222.196)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=orcl)));Persist Security Info=True;User ID=c##ysjyyds;Password=DBprinciple2022;";
+        OracleConnection con = new OracleConnection(connString);
+        public void openConn(GameAdd2LibResponse resp)
+        {
+            try
+            {
+                con.Open();
+                Console.WriteLine("Successfully connected to Oracle Database");
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Connection Failed!");
+                Console.WriteLine(e);
+                resp.result = -1;
+                con.Close();
+            }
+        }
+        
+        public int isGameInUserLib(string gid, string uid)
+        {
+            int ret = 0;
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = $"SELECT VIA_CDK FROM OWNERSHIP WHERE USER_ID='{uid}' AND GAME_ID='{gid}'";
+            try
+            {
+                OracleDataReader reader = cmd.ExecuteReader();
+                ret = reader.HasRows ? 1 : 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                ret = -1;
+            }
+
+            return ret;
+        }
+
+        public int AddGame(string gid, string uid)
+        {
+            int ret = 0;
+            OracleCommand cmd = con.CreateCommand();
+
+            /* add game to ownership */
+            cmd.CommandText = $"INSERT INTO OWNERSHIP VALUES('{uid}', '{gid}', 0)";
+            try
+            {
+                ret = cmd.ExecuteNonQuery();
+                if (ret == 0)
+                {
+                    cmd.CommandText = "ROLLBACK";
+                    cmd.ExecuteNonQuery();
+                    return ret;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                cmd.CommandText = "ROLLBACK";
+                cmd.ExecuteNonQuery();
+                return ret;
+            }
+
+            /* update number of games user owns */
+            cmd.CommandText = $"UPDATE GAME_USER SET GAME_NUM=GAME_NUM+1 WHERE ID='{uid}'";
+            try
+            {
+                ret = cmd.ExecuteNonQuery();
+                if (ret == 0)
+                {
+                    cmd.CommandText = "ROLLBACK";
+                    cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    cmd.CommandText = "COMMIT";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                ret = 0;
+                Console.WriteLine(e);
+                cmd.CommandText = "ROLLBACK";
+                cmd.ExecuteNonQuery();
+            }
+
+            return ret;
+        }
+
+        public GameAdd2LibResponse Add2Lib(GameAdd2LibRequest req)
+        {
+            GameAdd2LibResponse resp = new GameAdd2LibResponse();
+            resp.result = 0;
+            openConn(resp);
+            if (resp.result == -1)
+                return resp;
+
+            /* check if user and game exists and user hasn't owned the game yet */
+            Utils utils = new Utils();
+
+            //check if user exists
+            int user_exists = utils.isUserInDB(req.user_id, con);
+            if (user_exists != 1)
+            {
+                resp.result = user_exists;
+                con.Close();
+                return resp;
+            }
+
+            //check if game exists
+            int game_exists = utils.isGameInDB(req.game_id, con);
+            if (game_exists != 1)
+            {
+                resp.result = game_exists;
+                con.Close();
+                return resp;
+            }
+
+            //check if the user has already owns the game
+            int owns = isGameInUserLib(req.game_id, req.user_id);
+            if (owns != 0)
+            {
+                resp.result = owns == 1 ? 0 : -1;
+                con.Close();
+                return resp;
+            }
+
+            /* add game to user library */
+            int ret = AddGame(req.game_id, req.user_id);
+            if (ret != 1)
+                resp.result = -1;
+            else
+                resp.result = 1;
+
+            con.Close();
+            return resp;
+        }
+    }
+}
--- Back-End/Test_RegisterLogin/RegisterLogin/Utils.cs
+++ Back-End/Test_RegisterLogin/RegisterLogin/Utils.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
 
 namespace RegisterLogin
 {
@@ -25,6 +26,56 @@ namespace RegisterLogin
             /* calculate id with current sum of ids */
             string id = (int.Parse(sum) + 1).ToString().PadLeft(10, '0');
             return id;
+        }
+
+        public int isUserInDB(string uid, OracleConnection conn)
+        {
+            /*
+             * find whether user uid is in database
+             * @retrun: 1  -- uid in db
+             *          0  -- uid not in db
+             *          -1 -- connection failed
+             */
+            int ret;
+            OracleCommand cmd = conn.CreateCommand();
+            cmd.CommandText = $"SELECT NAME FROM GAME_USER WHERE ID='{uid}'";
+            try
+            {
+                OracleDataReader reader = cmd.ExecuteReader();
+                ret = reader.HasRows ? 1 : 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                ret = -1;
+            }
+
+            return ret;
+        }
+
+        public int isGameInDB(string gid, OracleConnection conn)
+        {
+            /*
+             * find whether game gid is in database
+             * @retrun: 1  -- gid in db
+             *          0  -- gid not in db
+             *          -1 -- connection failed
+             */
+            int ret;
+            OracleCommand cmd = conn.CreateCommand();
+            cmd.CommandText = $"SELECT NAME FROM GAME WHERE ID='{gid}'";
+            try
+            {
+                OracleDataReader reader = cmd.ExecuteReader();
+                ret = reader.HasRows ? 1 : 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                ret = -1;
+            }
+
+            return ret;
         }
     }
 }
