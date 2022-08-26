6dc8c1211afe9d7b6934ad6ae10a65851a55d5c8
GoodheadHe <1258066455@qq.com>
2022-08-26 12:22:09 +0800

    新增获取个人订单接口

---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Controllers/OrderGetController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RegisterLogin.Models;
+using RegisterLogin.Helpers;
+
+namespace RegisterLogin.Controllers
+{
+    [Route("api/user/getOrdersList")]
+    [ApiController]
+    public class OrderGetController : Controller
+    {
+        [HttpPost]
+        public OrderGetResponse Post([FromBody] OrderGetRequest req)
+        {
+            OrderGetHelper helper = new OrderGetHelper();
+            OrderGetResponse resp = helper.GetOrder(req);
+
+            return resp;
+        }
+    }
+}
