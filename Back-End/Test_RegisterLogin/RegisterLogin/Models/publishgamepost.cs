ba5122809f64ee6fee6d214f453c89d0c7acacb9
jjw2606781566 <87257275+jjw2606781566@users.noreply.github.com>
2022-08-30 21:55:19 +0800

    新增发布推送接口

---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Models/publishgamepost.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace publishgamepost.Models
+{
+    public class publishgamepostRequest
+    {
+        public IFormCollection files { get; set; }
+    }
+    public class publishgamepostResponse
+    {
+        public int result { get; set; }
+        public string message { get; set; }
+    }
+}
