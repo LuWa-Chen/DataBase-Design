e1a622b3bfde6b13e551ca9c4e9b1e9131376c2b
jjw2606781566 <87257275+jjw2606781566@users.noreply.github.com>
2022-09-01 10:37:00 +0800

    新增更新发行商信息接口

---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Models/PublisherProfile.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace PublisherProfile.Models
+{
+    public class PublisherProfileRequest
+    {
+        public IFormCollection files { get; set; }
+    }
+    public class PublisherProfileResponse
+    {
+        public int result { get; set; }
+        public string message { get; set; }
+    }
+}
