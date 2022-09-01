a3454a17dfeff9800296f5341277e4ad628b2eb1
GoodheadHe <1258066455@qq.com>
2022-09-01 13:51:26 +0800

    新增修改游戏信息接口

---
+++ Back-End/Test_RegisterLogin/RegisterLogin/Controllers/GameModifyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using RegisterLogin.Helpers;
+using RegisterLogin.Models;
+
+namespace RegisterLogin.Controllers
+{
+    [Route("api/publisher/changepublishgame")]
+    [ApiController]
+    public class GameModifyController : Controller
+    {
+        [HttpPost]
+        public GameModifyResponse Post([FromForm] GameModifyRequest req)
+        {
+            GameModifyResponse resp = new GameModifyResponse();
+            GameModifyHelper helper = new GameModifyHelper();
+            resp = helper.ModifyGame(req, Request);
+            return resp;
+        }
+    }
+}
